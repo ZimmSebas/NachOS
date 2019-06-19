565247437d9c0ee2316b59efff1066ccd8087705
Sebastián Zimmermann <zimmermannsebas@gmail.com>
2019-06-19 15:12:55 -0300

    Agrego Userland

--- code/userland/Test_Exec.c
+++ code/userland/Test_Exec.c
@@ -2,16 +2,17 @@
 
 
 int main(){
-  char pepe[]="cp";
-  char pepe2[]="TestCopy";
-  char pepe3[]="AAxpepe";
-  char *argv[4];
+  char pepe[]="args";
+  char pepe2[]="AA";
+  //~ char pepe3[]="AB";
+  //~ char *argv[4];
+  char *argv[3];
   argv[0] = pepe;
   argv[1] = pepe2;
-  argv[2] = pepe3;
-  argv[3] = 0;
+  //~ argv[2] = pepe3;
+  //~ argv[3] = 0;
+  argv[2] = 0;
   SpaceId id = Exec(argv,0);
-  //~ Write("pepe",5,1);
+  Write("pepe\n",6,1);
   Exit(0);
-  return 0;
 }
--- code/userland/args.c
+++ code/userland/args.c
@@ -2,28 +2,27 @@
 
 int main(int argc, char** argv){
 
-    char *arg1 = argv[1];
-	char *arg2 = argv[2];
+    //~ char *arg1 = argv[1];
+	//~ char *arg2 = argv[2];
 	
-	char s[10];
-	int leido = 0;
+	//~ char s[10];
+	//~ int leido = 0;
 	
-	OpenFileId id = 0, id2 = 0;
+	//~ OpenFileId id = 0, id2 = 0;
 	
 	//~ Create(arg1);
-	Create(arg2);
+	//~ Create(arg2);s
 
-	id = Open(arg1);
-	id2 = Open(arg2);
+	//~ id = Open(arg1);
+	//~ id2 = Open(arg2);
 	
-	do{
-	leido = Read(s,10,id);
-	Write(s,leido,id2);
-	}while(leido != 0);
-	Close(id);
-	Close(id2);
+	//~ do{
+	//~ leido = Read(s,10,id);
+	//~ Write(s,leido,id2);
+	//~ }while(leido != 0);
+	//~ Close(id);
+	//~ Close(id2);
 	
 	Exit(0);
-	return 0;	
 
 }
